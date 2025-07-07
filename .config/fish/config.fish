5dbee88d063648e8cc4cb186ba4756cd41605c6c
Alexandre FORET <foretalexandre37@gmail.com>
2025-07-07 22:56:53 +0200

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,2 @@
+set -x NVM_DIR $HOME/.nvm
+bass source $NVM_DIR/nvm.sh --no-use
---
+++ .config/fish/functions/code-wayld.fish
@@ -0,0 +1,4 @@
+function code-wayld --wraps='code --enable-features=UseOzonePlatform --ozone-platform=wayland' --description 'alias code-wayld=code --enable-features=UseOzonePlatform --ozone-platform=wayland'
+  code --enable-features=UseOzonePlatform --ozone-platform=wayland $argv
+        
+end
