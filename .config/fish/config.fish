2a1514269e9355d0b2416df46e6a1271f9fe47ee
Alexandre FORET <foretalexandre37@gmail.com>
2026-03-06 17:52:40 +0100

    update: tunar window mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
 set -x NVM_DIR $HOME/.nvm
 bass source $NVM_DIR/nvm.sh --no-use
 
-alias update="sudo yay -Suy"
+alias update="yay -Suy"
+
+set -g fish_cursor_default block
+set -g fish_cursor_insert block
+set -g fish_cursor_replace_one block
+set -g fish_cursor_visual block
